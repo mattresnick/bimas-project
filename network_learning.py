f5c2fedaefc2dff1598a96a548479e89091aa8a1
Patrick <talleyp93@gmail.com>
2020-03-28 11:54:56 -0600

    added all deriv functions

--- network_learning.py
+++ network_learning.py
@@ -15,7 +15,7 @@ def make_matrix(N):
     half_n = int(N/2)
     p1 = 0.7
     p2 = 0.2
-    w = 0.5
+    w = 0.1
     A = np.zeros((N,N))
     E = []
     for i in range(N):
@@ -32,7 +32,7 @@ def make_matrix(N):
                     E.append([i,j,w])
                 
     np.fill_diagonal(A, 0)
-    W = A*0.5
+    W = A*w
     return W,E
 
 def make_fire_rate(N,nu1,nu2):
@@ -41,8 +41,8 @@ def make_fire_rate(N,nu1,nu2):
     '''
     fire = {}
     for i in range(int(N/2)):
-        fire[i] = np.random.normal(nu1)
-        fire[i+int(N/2)] = np.random.normal(nu2)
+        fire[i] = max(0,np.random.normal(nu1))
+        fire[i+int(N/2)] = max(0,np.random.normal(nu2))
     return fire
 
 def oja_step(gamma,nu1,nu2,w,dt):
@@ -55,10 +55,32 @@ def oja_step(gamma,nu1,nu2,w,dt):
         w - synaptic weight at time t
         dt - time step
     oupt:
-        returns the new synaptic weight at time t+dt, where there is a hard max at 10
+        returns the new synaptic weight at time t+dt
     '''
-    dw = gamma*(nu1*nu2-w*nu1**2)
-    return w+dt*dw # just in case the weight blows up we cut if off at 10
+    dw = -w+gamma*(nu1*nu2-w*nu1**2)
+    return max(w+dt*dw,2)
+
+def fire_step(tau_r,nu_d,h,w,dt):
+    '''
+    
+    '''
+    N = len(nu_d.keys())
+    nu = np.zeros((N,1))
+    
+    for i in range(N):
+        nu[i] = nu_d[i]
+    out_nu = {}
+    nu = nu+dt/tau_r*(-nu+h+w@nu)
+ 
+    for i in range(N):
+        out_nu[i] = max(0,min(nu[i][0],2))
+    return out_nu
+
+def inpt_step(tau_m,h,R,I,dt):
+    '''
+    
+    '''
+    return h + dt/tau_m*(-h+R*I)
 
 def learn(E,W,f,gamma,dt):
     N = len(E)
@@ -66,41 +88,67 @@ def learn(E,W,f,gamma,dt):
         link = E[i]
         w_tmp = oja_step(gamma,f[link[0]],f[link[1]],link[2],dt)
         E[i][2] = w_tmp
-        W[link[0],link[1]] = w_tmp
+        try:
+            W[link[0],link[1]] = w_tmp
+        except ValueError:
+            print('w',w_tmp)
+            print('f[link[0]]',f[link[0]])
+            print('f[link[1]]',f[link[1]])
+            print('link[2]',link[2])
     return W,E
         
 
 if __name__ == "__main__":
     # parameters
     N = 50       # network size
-    nu1 = 1     # firing rate of 1st clique
-    nu2 = 1.5     # firing rate of 2nd clique
-    gamma = 2  # learning rate
-    T = 1000
+    nu1 = 0     # firing rate of 1st clique
+    nu2 = 0     # firing rate of 2nd clique
+    gamma = 0.1  # learning rate
+    tau_r = 1
+    tau_m = 1
+    h1 = 0
+    h2 = 0
+    I1 = 1
+    I2 = 1.5
+    R = 2
+    # time parameters
+    T = 10
     dt = 0.01
+
+    I = np.append(np.repeat(I1,N/2),np.repeat(I2,N/2))
+    h = np.append(np.repeat(h1,N/2),np.repeat(h2,N/2))
     nt = int(T/dt)+1
     half_n = int(N/2)
-
-    W0,E = make_matrix(N)
+    
+    W,E = make_matrix(N)
     f = make_fire_rate(N,nu1,nu2)
-    f2 = make_fire_rate(N,nu1/10,nu2)
-    W = np.zeros((N,N,nt))
-    W[:,:,0] = W0
     w_avg_1 =  np.zeros((nt,1))
     w_avg_2 = np.zeros((nt,1))
-    w_avg_1[0] = np.mean(W[:half_n,:half_n,0])
-    w_avg_2[0] = np.mean(W[half_n:,half_n:,0])
+    h_avg = np.zeros((nt,1))
+    f_avg = np.zeros((nt,1))
+    w_avg_1[0] = np.mean(W[:half_n,:half_n])
+    w_avg_2[0] = np.mean(W[half_n:N,half_n:N])
+    h_avg[0] = np.mean(h)
+    f_avg[0] = np.array(list(f.values())).mean()
     for i in range(1,nt):
-        W[:,:,i],E = learn(E,W[:,:,i-1],f,gamma,dt)
-        if i>nt/10:
-            f = f2
-
-        w_avg_1[i] = np.mean(W[:half_n,:half_n,i])
-        w_avg_2[i] = np.mean(W[half_n:,half_n:,i])
+        W_tmp,E = learn(E,W[:,:],f,gamma,dt)
+        f_tmp = fire_step(tau_r,f,h,W,dt)
+        h = inpt_step(tau_m,h,R,I,dt)
+        if i>=nt/20:
+            I[:half_n]=0
         
+        W = W_tmp
+        f = f_tmp
+        w_avg_1[i] = np.mean(W[:half_n,:half_n])
+        w_avg_2[i] = np.mean(W[half_n:,half_n:])
+        h_avg[i] = np.mean(h[0:half_n])
+        f_avg[i] = np.array(list(f.values())[0:half_n]).mean()
 
+    t_ls = np.linspace(0,T,nt)
     fig = plt.figure()
-    plt.plot(w_avg_1,label='$w_1$')
-    plt.plot(w_avg_2,label='$w_2$')
-    plt.legend(loc='upper left')
+    plt.plot(t_ls,w_avg_1,label='$w_1$')
+    plt.plot(t_ls,w_avg_2,label='$w_2$')
+    plt.plot(t_ls,h_avg,label='$h$')
+    plt.plot(t_ls,f_avg,label='$\\nu$')
+    plt.legend(loc='upper right')
     plt.show()
